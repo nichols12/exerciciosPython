e747bfbe4864927dfe7ada3b0b870c208e93d8d5
Nicholas Fonseca <nickpontoc@gmail.com>
2019-01-19 12:23:24 +0000

    adicionado resultado de novos exercícios

---
+++ EstruturaSequencial/exe6.py
@@ -0,0 +1,5 @@
+"""Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário."""
+import math
+
+raio = float(input("insira o raio do circulo"))
+print("o dobro da área deste circulo é:", (math.pi * raio * raio) * 2 )
---
+++ EstruturaSequencial/exe7.py
@@ -0,0 +1,3 @@
+"""Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário."""
+lado = float( input( "insira o tamanho de um dos lados do quadrado " ) )
+print( "o dobro da área do quadrado é",(pow(lado,2) * 2))
