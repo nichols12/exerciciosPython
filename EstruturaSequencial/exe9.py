c01ac6c0d8b81a7ab7ad340f1de8506573f5c9e9
Nicholas <nicholas@odysci.com>
2019-01-22 13:24:51 -0200

    efetuado exercicio 9

---
+++ EstruturaSequencial/exe9.py
@@ -0,0 +1,5 @@
+"""Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
+C = (5 * (F-32) / 9)."""
+
+temp = int(input("insira a temperatura em Farenheit"))
+print("temperatura em celsius é", (5 * (temp - 32) / 9))
